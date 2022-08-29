1e6074d6f9586b4fd1561c37616fac1f160fbf08
Luch <lperotti39@gmail.com>
2022-08-29 12:24:36 -0400

    Build functions in app.js to get random number for computer selection and log to the console to check results.

---
+++ app.js
@@ -0,0 +1,9 @@
+
+//A random number is generated and assigned as the computer's selection.
+function getComputerChoice () {
+    return Math.floor(Math.random()* 4);
+}
+
+console.log(getComputerChoice());
+
+//The random number is converted into a set of values - Rock Paper Scissors
