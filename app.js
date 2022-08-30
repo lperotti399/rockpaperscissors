22a8dde546003c09ea0bdd02905c1fc4bf4daea5
Luch <lperotti39@gmail.com>
2022-08-30 14:52:45 -0400

    Update app.js to add function that loops through five rounds of playRound function and prints winner to the console.

--- app.js
+++ app.js
@@ -26,32 +26,48 @@ function setComputerSelection () {
 
 let computerSelection = setComputerSelection();
 let getPlayerChoice = prompt("Rock Paper Scissors SHOOT!  (Enter your choice)")
-let playerSelection = getPlayerChoice[0].toUpperCase() + getPlayerChoice.toLowerCase().substring(1)
-
+let playerSelection = getPlayerChoice[0].toUpperCase() + getPlayerChoice.toLowerCase().substring(1);
+let userScore = parseInt(0);
+let computerScore = parseInt(0);
 function playRound(computerSelection, playerSelection) {
     
    if(computerSelection === 'Rock' && playerSelection === 'Scissors') {
+    ++computerScore;
     return 'Rock BEATS Scissors.  You LOSE!';
    }
    else if(computerSelection === 'Scissors' && playerSelection === 'Paper') {
+    ++computerScore;
     return 'Scissors BEATS Paper.  You LOSE!';
    }
    else if(computerSelection === 'Paper' && playerSelection === 'Rock') {
+    ++computerScore;
     return 'Paper BEATS Rock.  You LOSE!';
    }
    else if(computerSelection === 'Rock' && playerSelection === 'Paper') {
+    ++userScore;
     return 'Paper BEATS Rock.  You WIN!';
    }
    else if(computerSelection === 'Paper' && playerSelection === 'Scissors') {
+    ++userScore;
     return 'Scissors BEATS Paper.  You WIN!';
    }
    else if(computerSelection === 'Scissors' && playerSelection === 'Rock') {
+    ++userScore;
     return 'Rock BEATS Scissors.  You WIN!';
 }
 }
 let roundOutputText = playRound(computerSelection, playerSelection);
-console.log(roundOutputText);
+//console.log(roundOutputText);
 //console.log(playerSelection);
 //console.log(computerSelection);
 
+
+
+for(var i=0;i<5;i++){
+    let playerSelection = prompt("Pick a move");
+    let computerSelection = setComputerSelection()
+    console.log(roundOutputText)
+    console.log("your score = " + userScore);
+    console.log("Computer's score = " + computerScore);
+}
 
