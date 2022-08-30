c2e54abb6fe30bf9f3a858aa394b8d43490bd1af
Luch <lperotti39@gmail.com>
2022-08-30 12:11:19 -0400

    Add function that plays single round of rock paper scissors shoot and logs the winner and result to the console.

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ function getComputerChoice () {
 }
 
 let computerChoiceNum = getComputerChoice();
-console.log(computerChoiceNum);
-
 
 //The random number is converted into a set of values - Rock Paper Scissors
 function setComputerSelection () {
@@ -26,8 +24,34 @@ function setComputerSelection () {
     return computerChoiceText;
 }
 
-console.log(setComputerSelection());
-
+let computerSelection = setComputerSelection();
 let getPlayerChoice = prompt("Rock Paper Scissors SHOOT!  (Enter your choice)")
-let setPlayerSelection = getPlayerChoice[0].toUpperCase() + getPlayerChoice.toLowerCase().substring(1)
+let playerSelection = getPlayerChoice[0].toUpperCase() + getPlayerChoice.toLowerCase().substring(1)
+
+function playRound(computerSelection, playerSelection) {
+    
+   if(computerSelection === 'Rock' && playerSelection === 'Scissors') {
+    return 'Rock BEATS Scissors.  You LOSE!';
+   }
+   else if(computerSelection === 'Scissors' && playerSelection === 'Paper') {
+    return 'Scissors BEATS Paper.  You LOSE!';
+   }
+   else if(computerSelection === 'Paper' && playerSelection === 'Rock') {
+    return 'Paper BEATS Rock.  You LOSE!';
+   }
+   else if(computerSelection === 'Rock' && playerSelection === 'Paper') {
+    return 'Paper BEATS Rock.  You WIN!';
+   }
+   else if(computerSelection === 'Paper' && playerSelection === 'Scissors') {
+    return 'Scissors BEATS Paper.  You WIN!';
+   }
+   else if(computerSelection === 'Scissors' && playerSelection === 'Rock') {
+    return 'Rock BEATS Scissors.  You WIN!';
+}
+}
+let roundOutputText = playRound(computerSelection, playerSelection);
+console.log(roundOutputText);
+//console.log(playerSelection);
+//console.log(computerSelection);
+
 
