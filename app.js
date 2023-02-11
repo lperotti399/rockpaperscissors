ec402b5e020af1c3414429e98c08f87842bb50d7
Luch <lperotti39@gmail.com>
2023-02-11 07:32:58 -0500

    Added pscuedocode logic into app.js.  Fixed playerRound to properly add a score to each per each round.  Added console.log to tell the user who won after 5 rounds.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 
-//A random number is generated and assigned as the computer"s selection.
-//The random number is converted into a set of values - rock paper scissors
+
+
 function setComputerSelection () {
+//A random number is generated and assigned as the computer"s selection.
     let computerChoiceNum = Math.floor(Math.random()* 3);
     let computerChoiceText;
 
+//The random number is converted into a set of values - rock paper scissors
     switch (computerChoiceNum) {
         case 0:
             computerChoiceText = "rock";
@@ -19,47 +21,45 @@ function setComputerSelection () {
     return computerChoiceText;
 }
 
-//let computerSelection = setComputerSelection();
-//let getPlayerChoice = prompt("rock paper scissors SHOOT!  (Enter your choice)")
 
 function playRound(computerSelection, playerSelection) {
-    
+//Logic is determined to rank which value will beat which value
    if(computerSelection === "rock" && playerSelection === "scissors") {
-    computerScore++;
+    computerScore+=1;
     return "rock BEATS scissors.  You LOSE!";
    }
    else if(computerSelection === "rock" && playerSelection === "paper") {
-    userScore++;
+    userScore+=1;
     return "paper BEATS rock.  You WIN!";
    }
    else if(computerSelection === "rock" && playerSelection === "rock") {
-    userScore++;
+    userScore+=1;
     return "rock TIES rock.  You TIE!";
    }
 
    else if(computerSelection === "scissors" && playerSelection === "paper") {
-    computerScore++;
+    computerScore+=1;
     return "scissors BEATS paper.  You LOSE!";
    }
    else if(computerSelection === "scissors" && playerSelection === "rock") {
-    userScore++;
+    userScore+=1;
     return "rock BEATS scissors.  You WIN!";
 }
 else if(computerSelection === "scissors" && playerSelection === "scissors") {
-    userScore++;
+    userScore+=1;
     return "scissors TIES scissors.  You TIE!";
 }
    else if(computerSelection === "paper" && playerSelection === "rock") {
-    computerScore++;
+    computerScore+=1;
     return "paper BEATS rock.  You LOSE!";
    }
    
    else if(computerSelection === "paper" && playerSelection === "scissors") {
-    userScore++;
+    userScore+=1;
     return "scissors BEATS paper.  You WIN!";
    }
    else if(computerSelection === "paper" && playerSelection === "paper") {
-    userScore++;
+    userScore+=1;
     return "paper TIES paper.  You TIE!";
    }
 }
@@ -70,11 +70,22 @@ let userScore = parseInt(0);
 let computerScore = parseInt(0);
 
 for(var i=0;i<5;i++){
+//User is prompted to enter a value of Rock Paper or Scissors.
     let playerSelection = prompt("Pick a move");
     const computerSelection = setComputerSelection()
+//A single round of the game is played
     console.log(playRound(playerSelection, computerSelection))
     console.log("your score = " + userScore);
     console.log("Computer's score = " + computerScore);
 }
-if(computerScore> userScore) {console.log('You are the Loser')}
-else {console.log('You are the Winner')}
+if(computerScore> userScore) {
+    
+    console.log(`Your Score: ${userScore}`)
+    console.log(`Computer Score: ${computerScore}`)
+    console.log('You are the Loser')
+}
+else {
+    console.log(`Your Score: ${userScore}`)
+    console.log(`Computer Score: ${computerScore}`)
+    console.log('You are the Winner')
+}
